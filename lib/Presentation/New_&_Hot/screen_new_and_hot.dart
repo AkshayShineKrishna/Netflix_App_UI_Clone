db23d3953ade81ec6d14c6e038d8017b1c0317f9
AkshayShineKrishna <ask007kichu@gmail.com>
2023-05-29 19:37:31 +0530

    updated screen new and hot

--- lib/Presentation/New_&_Hot/screen_new_and_hot.dart
+++ lib/Presentation/New_&_Hot/screen_new_and_hot.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,10 +5,10 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:netflix/Application/hot_and_new/hot_and_new_bloc.dart';
 import 'package:netflix/Core/Colors/colors.dart';
 import 'package:netflix/Core/strings.dart';
+import 'package:netflix/Presentation/New_&_Hot/widgets/everyones_watching.dart';
 
 import '../../Core/constants.dart';
 import 'widgets/coming_soon_body.dart';
-import 'widgets/custom_icon_widget.dart';
 
 class ScreenNewAndHot extends StatelessWidget {
   const ScreenNewAndHot({Key? key}) : super(key: key);
@@ -170,84 +169,6 @@ class EveryOnesWatchingTabWidget extends StatelessWidget {
             separatorBuilder: (ctx, index) => kHeight,
             itemCount: filteredList.length);
       },
-    );
-  }
-}
-
-class EveronesWtchingBodyWidget extends StatelessWidget {
-  final String overview;
-  final String logoUrl;
-  final String imageUrl;
-  final String videoUrl;
-  const EveronesWtchingBodyWidget({
-    super.key,
-    required this.size,
-    required this.overview,
-    required this.logoUrl,
-    required this.imageUrl,
-    required this.videoUrl,
-  });
-
-  final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: size.width * 0.72,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: VideoWidget(
-              imageUrl: imageUrl,
-              videoUrl: videoUrl,
-            ),
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              kWidth,
-              SizedBox(
-                width: size.width * 0.23,
-                height: size.width * 0.1,
-                child: CachedNetworkImage(
-                  imageUrl: logoUrl,
-                  fit: BoxFit.scaleDown,
-                  alignment: Alignment.centerLeft,
-                ),
-              ),
-              const Spacer(),
-              const CustomIconWidget(
-                icon: Icons.share,
-                title: 'Share',
-              ),
-              kWidth,
-              const CustomIconWidget(
-                icon: Icons.add,
-                title: 'My List',
-              ),
-              kWidth,
-              const CustomIconWidget(
-                icon: Icons.play_arrow_rounded,
-                title: 'Play',
-              ),
-              kWidth
-            ],
-          ),
-          kHeight,
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Text(
-              overview,
-              textAlign: TextAlign.left,
-              style: const TextStyle(color: kGreyFont, fontSize: 12.5),
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-            ),
-          )
-        ],
-      ),
     );
   }
 }
